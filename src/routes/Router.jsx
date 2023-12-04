eb2ce385e2703f70753c7117a9bc65113de4f3b9
ben <benoved9@gmail.com>
2023-12-04 21:16:42 +0200

    protecredRoutes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,6 +5,8 @@ import LoginPage from "../pages/LoginPage";
 import AdminZonePage from "../pages/AdminZonePage";
 import FavoritesPage from "../pages/FavoritesPage";
 import UserProfilePage from "../pages/UserProfilePage";
+import ProtectedRoute from "../components/ProtectedRoute";
+import SuperProtectedRoute from "../components/SuperProtectedRoute";
 
 import ROUTES from "./ROUTES";
 import EditCoinPage from "../pages/EditCoinPage";
@@ -15,10 +17,32 @@ const Router = () => {
       <Route path={ROUTES.HOME} element={<HomePage />} />
       <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
       <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-      <Route path={ROUTES.ADMINZONE} element={<AdminZonePage />} />
-      <Route path={ROUTES.FAV} element={<FavoritesPage />} />
-      <Route path={ROUTES.PROFILE} element={<UserProfilePage />} />
-      <Route path="editcoin/:id" element={<EditCoinPage />} />
+      {/* <Route path={ROUTES.ADMINZONE} element={<AdminZonePage />} /> */}
+      <Route
+        path={ROUTES.ADMINZONE}
+        element={
+          <SuperProtectedRoute isAdmin={true} element={<AdminZonePage />} />
+        }
+      />
+
+      <Route
+        path={ROUTES.FAV}
+        element={<ProtectedRoute element={<FavoritesPage />} />}
+      />
+      <Route
+        path={ROUTES.PROFILE}
+        element={<ProtectedRoute element={<UserProfilePage />} />}
+      />
+
+      {/* <Route path="editcoin/:id" element={<EditCoinPage />} /> */}
+
+      <Route
+        path="editcoin/:id"
+        element={
+          <SuperProtectedRoute isAdmin={true} element={<EditCoinPage />} />
+        }
+      />
+
       <Route path="*" element={<h1>404</h1>} />
     </Routes>
   );
